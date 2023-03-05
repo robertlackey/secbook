371ba17f4608e54b90709fa52ec2b34a32147fe8
Robert Lackey <rlackey4@gmail.com>
2023-03-04 17:55:42 -0800

    Update extract_mitre.py

--- extract_mitre.py
+++ extract_mitre.py
@@ -2,70 +2,30 @@ import requests
 import json
 
 class extract_mitre:
-	def __init__(self):
-		#defining get request to pull data from github
-		r = requests.get("https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json")
-		try:
-			self.json_data = r.json()
-		except:
-			pass
+    def __init__(self):
+        # defining get request to pull data from github
+        r = requests.get("https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json")
+        try:
+            self.json_data = r.json()
+        except:
+            pass
 
-	def get_id(self, id):
-		#getting the mitre id based on the mitre_id entered in playbook form
-		for value in self.json_data['objects']:
-			try:
-				if value['external_references'][0]['external_id']==id:
-					mitre_id = value['external_references'][0]['external_id']
-					return mitre_id
-			except:
-				pass
-
-	def get_name(self, id):
-		#getting the mitre name based on the mitre_id entered in playbook form
-		for value in self.json_data['objects']:
-			try:
-				if value['external_references'][0]['external_id']==id:
-					name = value['name']
-					return name
-			except:
-				pass
-
-	def get_description(self, id):
-		#getting the description based on the mitre_id entered in playbook form
-		for value in self.json_data['objects']:
-			try:
-				if value['external_references'][0]['external_id']==id:
-					description = value['description']
-					return description
-			except:
-				pass
-
-	def get_url(self, id):
-		#getting the mitre url based on the mitre_id entered in playbook form
-		for value in self.json_data['objects']:
-			try:
-				if value['external_references'][0]['external_id']==id:
-					url = value['external_references'][0]['url']
-					return url
-			except:
-				pass
-
-	def get_platforms(self, id):
-		#getting the mitre OS/platform info based on the mitre_id entered in playbook form
-		for value in self.json_data['objects']:
-			try:
-				if value['external_references'][0]['external_id']==id:
-					platforms = ', '.join(value['x_mitre_platforms'])
-					return platforms
-			except:
-				pass
-				
-	def get_kill_chain_phase(self, id):
-		for value in self.json_data['objects']:
-			try:
-				if value['external_references'][0]['external_id']==id:
-					platforms = value['kill_chain_phases'][0]['phase_name']
-					return platforms
-			except:
-				pass
-		
+    def get_attribute(self, mitre_id, attribute):
+        # getting the specified attribute based on the mitre_id entered in playbook form
+        for json_object in self.json_data['objects']:
+            try:
+                if json_object['external_references'][0]['external_id'] == mitre_id:
+                    if attribute == 'id':
+                        return json_object['external_references'][0]['external_id']
+                    elif attribute == 'name':
+                        return json_object['name']
+                    elif attribute == 'description':
+                        return json_object['description']
+                    elif attribute == 'url':
+                        return json_object['external_references'][0]['url']
+                    elif attribute == 'platforms':
+                        return ', '.join(json_object['x_mitre_platforms'])
+                    elif attribute == 'kill_chain_phase':
+                        return json_object['kill_chain_phases'][0]['phase_name']
+            except:
+                pass
